6afe3df98c254a4252e1a50b0e5eb797e1ad1639
Mmd-Amir <xmmd.ir@gmail.com>
2025-12-26 15:04:08 -0600

    fix2

--- Refactorinstall/install_parts/arguments.sh
+++ Refactorinstall/install_parts/arguments.sh
@@ -1,46 +1,44 @@
-\
-    #!/usr/bin/env bash
-    # Mirza installer module: arguments
-    # Safe under `set -u` (nounset)
+if [[ -n "${__MIRZA_ARGUMENTS_LOADED:-}" ]]; then
+  return 0
+fi
+__MIRZA_ARGUMENTS_LOADED=1
 
-    if [[ -n "${__MIRZA_ARGUMENTS_LOADED:-}" ]]; then
-      return 0
-    fi
-    __MIRZA_ARGUMENTS_LOADED=1
-
-    process_arguments() {
-        local arg1="${1-}"
-        local arg2="${2-}"
-        local version=""
-
-        case "$arg1" in
-            -v*)
-                version="${arg1#-v}"
-                if [[ -n "$version" ]]; then
-                    install_bot "-v" "$version"
+# shellcheck shell=bash
+# Mirza installer module: arguments
+# This file is sourced by install.sh
+process_arguments() {
+    # Use "${N-}" so this module works with `set -u` (nounset)
+    # even when the caller doesn't pass optional positional args.
+    local arg1="${1-}"
+    local arg2="${2-}"
+    local version=""
+    case "$arg1" in
+        -v*)
+            version="${arg1#-v}"
+            if [ -n "$version" ]; then
+                install_bot "-v" "$version"
+            else
+                if [ -n "$arg2" ]; then
+                    install_bot "-v" "$arg2"
                 else
-                    if [[ -n "$arg2" ]]; then
-                        install_bot "-v" "$arg2"
-                    else
-                        echo -e "\033[31m[ERROR]\033[0m Please specify a version with -v (e.g., -v 4.11.1)"
-                        exit 1
-                    fi
+                    echo -e "\033[31m[ERROR]\033[0m Please specify a version with -v (e.g., -v 4.11.1)"
+                    exit 1
                 fi
-                ;;
-            -beta|--beta)
-                install_bot "-beta"
-                ;;
-            -update)
-                update_bot "$arg2"
-                ;;
-            *)
-                show_menu
-                ;;
-        esac
-    }
+            fi
+            ;;
+        -beta)
+            install_bot "-beta"
+            ;;
+        --beta)
+            install_bot "-beta"
+            ;;
+        -update)
+            update_bot "$arg2"
+            ;;
+        *)
+            show_menu
+            ;;
+    esac
+}
 
-    # Call immediately if this module is sourced as last step
-    # (Entry script also calls process_arguments, but this keeps backward-compat if module is used alone)
-    if [[ "${MIRZA_ARGUMENTS_AUTORUN:-1}" == "1" ]]; then
-      process_arguments "${1-}" "${2-}"
-    fi
+process_arguments "${1-}" "${2-}"
